6341060ff7194f0785597f3f04998518ea13dfed
Navin Developer <dev@navin.local>
2026-04-23 12:29:40 +0100

    feat(#599): Add Prediction repository to MarketsModule for getPredictionStats

--- backend/src/markets/markets.module.ts
+++ backend/src/markets/markets.module.ts
@@ -4,17 +4,24 @@ import { Market } from './entities/market.entity';
 import { Comment } from './entities/comment.entity';
 import { MarketTemplate } from './entities/market-template.entity';
 import { UserBookmark } from './entities/user-bookmark.entity';
+import { Prediction } from '../predictions/entities/prediction.entity';
 import { MarketsService } from './markets.service';
 import { MarketsController } from './markets.controller';
 import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Market, Comment, MarketTemplate, UserBookmark]),
+    TypeOrmModule.forFeature([
+      Market,
+      Comment,
+      MarketTemplate,
+      UserBookmark,
+      Prediction,
+    ]),
     UsersModule,
   ],
   controllers: [MarketsController],
   providers: [MarketsService],
   exports: [MarketsService, TypeOrmModule],
 })
-export class MarketsModule {}
+export class MarketsModule { }
